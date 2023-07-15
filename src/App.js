6f6c8477793e21d7c344a542c29d6dcad15dea86
Gbenga Fakorede <temris123@gmail.com>
2023-07-15 01:32:59 +0100

    second

--- src/App.js
+++ src/App.js
@@ -10,13 +10,12 @@ const App = () => {
   return (
     <div>
       <Steps />
-      <Steps />
+      {/* <Steps /> */}
     </div>
   );
 };
 const Steps = () => {
   const [step, setStep] = useState(1);
-
   const [isOpen, setIsOpen] = useState(true);
 
   // const [test, setTest] = useState({ name: "Gbenga" });
@@ -44,27 +43,50 @@ const Steps = () => {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span> Previous{" "}
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+};
+
+const StepMessage = ({ step, children }) => {
+  return (
+    <div className="message">
+      <h3> Step {step}</h3>
+      {children}
+    </div>
+  );
+};
+const Button = ({ textColor, bgColor, onClick, children }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 };
 
